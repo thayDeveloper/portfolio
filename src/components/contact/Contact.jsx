241f2aa6e1e24eff31d38913b19cc6ccdb955dfe
thaydeveloper <thaydeveloper26@gmail.com>
2023-03-02 13:28:32 -0300

    componente de contato finalizado, foi adicionado a bibilhoteca emailjs para trabalhar com o envio de emails e de projetos futuros

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import "./contact.css";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "service_bgqe248",
+      "template_f0icwsd",
+      form.current,
+      "3_WjBuR6thktVshdT"
+    );
+
+    e.target.reset();
+  };
+
   return (
     <section className="contact section" id="contact">
       <h2 className="section__title">Entre Em Contato</h2>
@@ -55,7 +71,8 @@ const Contact = () => {
 
         <div className="contact__content">
           <h3 className="contact__title">Descreva Seu Projeto</h3>
-          <form className="contact__form">
+
+          <form ref={form} onSubmit={sendEmail} className="contact__form">
             <div className="contact__form-div">
               <label className="contact__form-tag">Nome</label>
               <input
